ebfceec29bfba15ba1b6de826814157d696a08aa
acaleza <adrian.calvo1@gmail.com>
2023-06-06 16:23:33 +0200

    Añadido componente twimp-card

--- Tweempus/src/app/dashboard/dashboard.component.ts
+++ Tweempus/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TwimpCardComponent } from '../shared/twimp/twimp-card/twimp-card.component';
 
 @Component({
   selector: 'tweempus-dashboard',
--- Tweempus/src/app/dashboard/dashboard.module.ts
+++ Tweempus/src/app/dashboard/dashboard.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-
-
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports:[DashboardComponent]
+    declarations: [
+        DashboardComponent
+    ],
+    exports: [DashboardComponent],
+    imports: [
+        CommonModule,
+        SharedModule
+    ]
 })
 export class DashboardModule { }
