b475f6c03dbd0254c9425f28bbf2f827107a54ec
acaleza <adrian.calvo1@gmail.com>
2023-06-07 23:38:09 +0200

    Refactor con property binding

--- Tweempus/src/app/shared/twimp/twimp-list/twimp-list.component.ts
+++ Tweempus/src/app/shared/twimp/twimp-list/twimp-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Twimp } from '../twimp.model';
+import { Author } from '../../author/author.model';
 
 @Component({
   selector: 'tweempus-twimp-list',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./twimp-list.component.css']
 })
 export class TwimpListComponent {
+  text: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur sodales libero, sit amet posuere arcu consectetur ut. Nam volutpat ligula ac nunc consectetur vestibulum.';
+  authors: Author[] = [];
+  twimps: Twimp[] = [];
+
+  ngOnInit() {
+    this.authors.push(new Author('1'));
+    this.twimps.push(new Twimp('1', '', this.authors[0], this.text, '01/01/2023'));
+    this.twimps.push(new Twimp('2', '', this.authors[0], this.text, '01/01/2023'));
+    this.twimps.push(new Twimp('3', '', this.authors[0], this.text, '01/01/2023'));
+    this.twimps.push(new Twimp('4', '', this.authors[0], this.text, '01/01/2023'));
+  }
 
 }
