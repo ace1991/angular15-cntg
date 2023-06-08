0bad7e78d05eb867be9b120c2abff53ca278ceca
acaleza <adrian.calvo1@gmail.com>
2023-06-08 21:25:31 +0200

    Añadida función marcar favorito twimp

--- Tweempus/src/app/shared/twimp/twimp-card/twimp-card.component.ts
+++ Tweempus/src/app/shared/twimp/twimp-card/twimp-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Twimp } from '../twimp.model';
 
 @Component({
@@ -8,5 +8,12 @@ import { Twimp } from '../twimp.model';
 })
 export class TwimpCardComponent {
   @Input() twimp!:Twimp;
+  @Output() eventoLike = new EventEmitter();
+
+  marcarFavorito(): void {
+    this.twimp.favorite ? this.twimp.favorite=false : this.twimp.favorite=true;
+    console.log("ID Twimp: " + this.twimp.id + " - Fav: " + this.twimp.favorite)
+    this.eventoLike.emit(this.twimp.favorite);
+  }
 
 }
